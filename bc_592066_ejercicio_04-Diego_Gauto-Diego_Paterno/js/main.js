f81c2f4dc48758ae57efc4af088f2b6bd6bc1b39
diego <89492190+diegopaterno@users.noreply.github.com>
2022-06-02 15:33:30 -0300

    Add files via upload

---
+++ bc_592066_ejercicio_04-Diego_Gauto-Diego_Paterno/js/main.js
@@ -0,0 +1,23 @@
+let nombre 
+            
+nombre = prompt('ingrese su nombre')
+console.log(nombre)
+
+let genero
+genero = prompt('elija a.masculino b. femenino c.binario')
+console.log(genero)
+
+if (genero == 'a'){
+    console.log('hola masculino')
+} if (genero =='b'){
+    console.log('hola femenino')
+
+}if (genero == 'c') {
+  console.log('hola binario')
+}
+
+
+
+
+
+
